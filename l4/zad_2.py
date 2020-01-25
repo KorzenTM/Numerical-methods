96f86756b3d636c90a579659d1097dca41745ef5
KorzenTM <mateuszkorzeniowski98@gmail.com>
2020-01-25 20:42:59 +0100

    Improve Newton's Methode

--- l4/zad_2.py
+++ l4/zad_2.py
@@ -1,25 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 19 10:06:10 2019
 
-@author: mateu
-"""
+def newton_method(number, number_iters = 500):
+    a = float(number) 
+    for i in range(number_iters): 
+        number = (0.2) * (4*number + a / number**4) 
+	  # x_(n+1) = 0.5 * (x_n +a / x_n)
+    return number
 
-#Metoda Newtona
-
-
-#Program liczący pierwiastek piątego stopnia za pomocą metody Newtona
-
-def f(x,num): #funkcja pierwiastkowa
-    return x**5-num
-def f_prim(x): #pochodna pierwszej funkcji
-    return 5**x
 
 num=float(input("Enter a value:"))
-x0=num-1 #przyblizamy x0 do liczby bliskiej szukanej
-for i in range(1000): #iloć iteracji = dokładnosc
-    temp=x0-(f(x0,num)/f_prim(x0))
-#    print(temp)
-    x0=temp
-    
-print(temp) #wypisujemy ostatnią wartosc bedaca najbardziej prawidlowa
+print(newton_method(num))
+
